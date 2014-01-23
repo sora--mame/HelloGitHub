d8107685e8b2e24c441e1802116259e7a850f31a
sora- mame <s1531071@eng.kitakyu-u.ac.jp>
2014-01-23 13:10:11 +0900

    1310

--- HelloGitHub/ViewController.h
+++ HelloGitHub/ViewController.h
@@ -9,5 +9,41 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+{
+ IBOutlet UIButton *rei;
+ IBOutlet UIButton *ten;
+ IBOutlet UIButton *iti;
+ IBOutlet UIButton *ni;
+ IBOutlet UIButton *san;
+ IBOutlet UIButton *yon;
+ IBOutlet UIButton *go;
+ IBOutlet UIButton *roku;
+ IBOutlet UIButton *nana;
+ IBOutlet UIButton *hati;
+ IBOutlet UIButton *kyuu;
+ IBOutlet UIButton *c;
+ IBOutlet UIButton *tasu;
+ IBOutlet UIButton *hiku;
+ IBOutlet UIButton *kakeru;
+ IBOutlet UIButton *waru;
+ IBOutlet UIButton *ihugou;
+ IBOutlet UIButton *ikoru;
+ IBOutlet UIButton *hatena;
+ IBOutlet UITextField *kekka;
+ IBOutlet UIImageView *tek;
+ IBOutlet UIButton *y;
+ 
+    BOOL stratInput;
+    int currentValue;
+    int operation;
+}
+
+- (IBAction)suuzi_down:(id)sender;
+- (IBAction)tasuhiku_down:(id)sender;
+- (IBAction)ikoru_down:(id)sender;
+- (IBAction)c_down:(id)sender;
+- (IBAction)hatena_down:(id)sender;
+- (IBAction)y_down:(id)sender;
 
 @end
+
--- HelloGitHub/ViewController.m
+++ HelloGitHub/ViewController.m
@@ -13,17 +13,105 @@
 @end
 
 @implementation ViewController
+UIImage *gu_img;
+int k,z;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-}
+    stratInput = YES;
+    currentValue = 0;
+    gu_img = [UIImage imageNamed:@"oh.png"];}
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)suuzi_down:(id)sender{
+    UIButton *b = (UIButton * )sender;
+    z=0;
+    if( stratInput){
+        if(b.tag == 0){
+            if(k >= 1){
+             b.tag=0;
+            }else{
+                return;
+            }
+        }
+        
+         kekka.text = [NSString stringWithFormat:@"%d",b.tag];
+            stratInput =  NO;
+    }
+    else
+    {
+        kekka.text = [NSString stringWithFormat:@"%@%d",kekka.text,b.tag];
+    }
+}
 
+- (IBAction)tasuhiku_down:(id)sender
+{   k++;
+    if(k>1){
+     if (operation == 0){
+        currentValue += [kekka.text intValue];
+     }else if(operation == 1)
+     {
+        currentValue -= [kekka.text intValue];
+     }else if (operation == 2){
+        currentValue *= [kekka.text intValue];
+     }else if(operation == 3)
+     {
+        currentValue /= [kekka.text intValue];
+     }
+        kekka.text = [NSString stringWithFormat:@"%d",currentValue];
+        stratInput = YES;
+    }
+    UIButton *b = (UIButton *)sender;
+    currentValue = [kekka.text intValue];
+    operation = b.tag;
+    if (operation == 4)
+    {
+        currentValue = (-1)*currentValue;
+        kekka.text = [NSString stringWithFormat:@"%d",currentValue];
+    }
+    stratInput= YES;
+}
+
+
+- (IBAction)ikoru_down:(id)sender
+{   k=0;z++;
+   if(z<2){
+    if (operation == 0){
+        currentValue += [kekka.text intValue];
+    }else if(operation == 1)
+    {
+         currentValue -= [kekka.text intValue];
+    }else if (operation == 2){
+         currentValue *= [kekka.text intValue];
+    }else if (operation == 3)
+    {
+         currentValue /= [kekka.text intValue];
+    }
+    kekka.text = [NSString stringWithFormat:@"%d",currentValue];
+    stratInput = YES;
+  }
+    kekka.text = [NSString stringWithFormat:@"%d",currentValue];
+}
+
+- (IBAction)hatena_down:(id)sender;
+{
+    [tek setImage:gu_img];
+}
+
+- (IBAction)y_down:(id)sender;{
+    [tek setImage:nil];}
+
+- (IBAction)c_down:(id)sender
+{
+    k=0;
+    kekka.text = @"0";
+    stratInput = YES;
+    //currentValue=0;
+}
 @end
